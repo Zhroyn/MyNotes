3e724c8ccb569847ac6f0cd1d84714817e6f1479
Zhroyn <hr.zheng@outlook.com>
2022-11-26 15:48:05 +0800

    add cs61b-fa22

---
+++ cs61b-fa22/lab01/LeapYear.java
@@ -0,0 +1,35 @@
+/**
+ * Class that determines whether or not a year is a leap year.
+ *
+ * @author YOUR NAME HERE
+ */
+public class LeapYear {
+
+    /**
+     * Update this comment to describe what this method does.
+     */
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+    }
+
+    /**
+     * Calls isLeapYear to print correct statement.
+     */
+    private static void checkLeapYear(int year) {
+        if (isLeapYear(year)) {
+            System.out.printf("%d is a leap year.\n", year);
+        } else {
+            System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
+
+    /**
+     * Basic (Optional) Sanity Tests
+     */
+    public static void main(String[] args) {
+        System.out.println("Checking the year 2000, which should be a leap year:");
+        checkLeapYear(2000);
+        System.out.println("Checking the year 1700, which should be not a leap year:");
+        checkLeapYear(1700);
+    }
+}
