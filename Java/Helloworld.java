1de0c3f95ffdd442899c619e5b7e4379744db996
Zhroyn <hr.zheng@outlook.com>
2022-12-10 15:05:29 +0800

    dalete cs61b-fa22, scheme

--- Java/Helloworld.java
+++
@@ -1,9 +0,0 @@
-public class Helloworld {
-    public static void main(String[] args) {
-        String[] s = new String[3];
-        s[1] = "sadsad";
-        String str = "fdbvcx";
-        System.out.println(s[1]);
-        System.out.println(str);
-    }
-}
