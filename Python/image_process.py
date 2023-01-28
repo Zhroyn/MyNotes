a020c8f2339d9b0de3c26cb2277a6cf44759438f
Zhroyn <hr.zheng@outlook.com>
2023-01-28 20:41:39 +0800

    add image_progress.py

---
+++ Python/image_process.py
@@ -0,0 +1,42 @@
+import cv2
+
+def remove_stains(impath, outpath):
+    im = cv2.imread(impath)
+    if im is None:
+        print("File open failed :", impath)
+    else:
+        h, w = im.shape[:2]
+        for y in range(h):
+            for x in range(w):
+                if (int(im[y, x, 0])
+                  + int(im[y, x, 1])
+                  + int(im[y, x, 1]) > 420):
+                    im[y, x] = 255
+        im = cv2.GaussianBlur(im, (3, 3), 0, 0)
+        cv2.imwrite(outpath, im)
+
+def change_background(impath, outpath):
+    im = cv2.imread(impath)
+    if im is None:
+        print("File open failed :", impath)
+    else:
+        h, w = im.shape[:2]
+        bg_color = im[10, 10].copy()
+        print(bg_color)
+        delta = 25
+        for y in range(h):
+            for x in range(w):
+                b, g, r = im[y, x]
+                if (abs(int(b) - int(bg_color[0])) <= delta
+                and abs(int(g) - int(bg_color[1])) <= delta
+                and abs(int(r) - int(bg_color[2])) <= delta):
+                    im[y, x] = 255
+        im = cv2.GaussianBlur(im, (3, 1), 0, 0)
+        cv2.imwrite(outpath, im)
+
+
+remove_stains("C:/Users/hrzhe/Pictures/Calculus/500cef37a1fb8886d4c4ef664877a86.jpg",
+            "C:/Users/hrzhe/Pictures/Calculus/out.jpg")
+
+# change_background("C:/Users/hrzhe/Pictures/Calculus/5.png", 
+#                 "C:/Users/hrzhe/Pictures/Calculus/5.jpg")
