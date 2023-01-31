e03a792113e9007d6d6cf89834c35db35908e331
Zhroyn <hr.zheng@outlook.com>
2023-01-31 16:24:42 +0800

    image_process.py : change API from FileInFileOut to ImgInImgOut

--- Python/image_process.py
+++ Python/image_process.py
@@ -21,97 +21,80 @@ def get_paths(prefix, name, suffix, out_suffix=""):
     outpath = impath_add_suffix(impath, out_suffix)
     return impath, outpath
     
-def change_quailty(impath, outpath, scale):
+def change_quailty(src, scale):
     if scale == 1 or scale <= 0:
         pass
-    im = cv2.imread(impath)
-    if im is None:
-        raise FileNotFoundError(impath)
     else:
         if scale < 1:
             interpolation = cv2.INTER_AREA
         else:
             interpolation = cv2.INTER_CUBIC
-        im = cv2.resize(im, None, fx=scale, fy=scale,
+        im = cv2.resize(src, None, fx=scale, fy=scale,
                         interpolation=interpolation)
-        cv2.imwrite(outpath, im)
+        return im
 
-def enhance_color(impath, outpath, factor):
-    im = Image.open(impath)
-    out_ext = os.path.splitext(outpath)[1][1:]
-    if out_ext == "jpg" and im.mode != "RGB":
-        im = im.convert("RGB")
+def enhance_color(src, factor):
+    im = Image.fromarray(src)
     enh_col = ImageEnhance.Color(im)
     im = enh_col.enhance(factor)
-    im.save(outpath)
+    return np.asarray(im)
 
-def enhance_sharpness(impath, outpath, factor):
-    im = Image.open(impath)
-    out_ext = os.path.splitext(outpath)[1][1:]
-    if out_ext == "jpg" and im.mode != "RGB":
-        im = im.convert("RGB")
+def enhance_sharpness(src, factor):
+    im = Image.fromarray(src)
     enh_sha = ImageEnhance.Sharpness(im)
     im = enh_sha.enhance(factor)
-    im.save(outpath)
+    return np.asarray(im)
 
-def gaussian_blur(impath, outpath, sigma):
-    im = cv2.imread(impath)
-    im = cv2.GaussianBlur(im, None, sigma)
-    cv2.imwrite(outpath, im)
+def gaussian_blur(src, sigma):
+    return cv2.GaussianBlur(src, None, sigma)
 
-def remove_background(impath, outpath, delta):
-    im = cv2.imread(impath)
-    if im is None:
-        raise FileNotFoundError(impath)
-    else:
-        h, w = im.shape[:2]
-        bg_color = im[10, 10].copy()
-        print("Background Color :",bg_color)
-        for y in range(h):
-            for x in range(w):
-                b, g, r = im[y, x]
-                if (abs(int(b) - int(bg_color[0])) <= delta
-                and abs(int(g) - int(bg_color[1])) <= delta
-                and abs(int(r) - int(bg_color[2])) <= delta):
-                    im[y, x] = 255
-        cv2.imwrite(outpath, im)
+def remove_background(src, delta):
+    h, w = src.shape[:2]
+    bg_color = src[5, 5].copy()
+    print("Background Color :",bg_color)
+    for y in range(h):
+        for x in range(w):
+            b, g, r = src[y, x]
+            if (abs(int(b) - int(bg_color[0])) <= delta
+            and abs(int(g) - int(bg_color[1])) <= delta
+            and abs(int(r) - int(bg_color[2])) <= delta):
+                src[y, x] = 255
+    return src
 
-def remove_stains(impath, outpath, threshold):
-    im = cv2.imread(impath)
-    if im is None:
-        raise FileNotFoundError(impath)
-    else:
-        h, w = im.shape[:2]
-        threshold = 3 * threshold
-        for y in range(h):
-            for x in range(w):
-                if (int(im[y, x, 0])
-                + int(im[y, x, 1])
-                + int(im[y, x, 1]) > threshold):
-                    im[y, x] = 255
-        cv2.imwrite(outpath, im)
+def remove_stains(src, threshold):
+    h, w = src.shape[:2]
+    threshold = 3 * threshold
+    for y in range(h):
+        for x in range(w):
+            if (int(src[y, x, 0])
+            + int(src[y, x, 1])
+            + int(src[y, x, 1]) > threshold):
+                src[y, x] = 255
+    return src
 
 def image_process(impath, outpath, argv):
+    im = cv2.imread(impath)
+    if im is None:
+        raise FileNotFoundError(impath)
+    
     operations = argv.split()
-    n = 0
     for operation in operations:
         func, colon, arg = operation.partition(":")
         arg = float(arg)
-        if n > 0:
-            impath = outpath
         if func == "cq":
-            change_quailty(impath, outpath, arg)
+            im = change_quailty(im, arg)
         elif func == "col":
-            enhance_color(impath, outpath, arg)
+            im = enhance_color(im, arg)
         elif func == "sha":
-            enhance_sharpness(impath, outpath, arg)
+            im = enhance_sharpness(im, arg)
         elif func == "rbg":
-            remove_background(impath, outpath, arg)
+            im = remove_background(im, arg)
         elif func == "rst":
-            remove_stains(impath, outpath, arg)
+            im = remove_stains(im, arg)
         elif func == "gau":
-            gaussian_blur(impath, outpath, arg)
-        n += 1
+            im = gaussian_blur(im, arg)
+    
+    cv2.imwrite(outpath, im)
     print(outpath, "ended.")
 
 
@@ -124,12 +107,12 @@ clear_bg = " sha:2 cq:2 sha:2 cq:2 rbg:25 rst:200 col:1.7 cq:0.5"
 clear_bg_blur = " cq:2 sha:2 cq:2 rbg:25 rst:200 col:1.7 cq:0.5"
 
 src_dir = "C:/Users/hrzhe/Pictures/Calculus/"
-argv = "rst:200 gau:1"
-out_suffix = "_"
+argv = clear_bg
 out_suffix = argv.replace(":", "").replace(".", "")
+out_suffix = "_"
 # l = os.listdir(src_dir)
 l = [2, 3, 4, 5, 6, 8, 9, 11, 12]
-l = [7]
+l = [2]
 
 for i in l:
     impath, outpath = get_paths(src_dir, str(i), ".png", out_suffix)
