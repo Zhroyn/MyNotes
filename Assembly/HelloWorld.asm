b9331da302555d1148e9f265b24721c8c0fb96a8
Zhroyn <hr.zheng@outlook.com>
2023-03-03 23:32:34 +0800

    add HelloWorld.asm

---
+++ Assembly/HelloWorld.asm
@@ -0,0 +1,20 @@
+DSEG    SEGMENT
+MESS    DB   'Hello,World!' ,0DH,0AH,24H
+DSEG    ENDS
+
+SSEG    SEGMENT PARA STACK 
+        DW  256 DUP(?)
+SSEG    ENDS
+
+CSEG    SEGMENT
+        ASSUME  CS:CSEG,DS:DSEG
+BEGIN:  MOV AX,DSEG
+        MOV DS,AX
+        MOV DX,OFFSET MESS
+        MOV AH,9
+
+        INT 21H
+        MOV AH,4CH
+        INT 21H
+CSEG    ENDS
+        END  BEGIN
