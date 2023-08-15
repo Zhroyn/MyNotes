7807edc29de68d59a1b81471c0a8eceee886fe27
Zhroyn <hr.zheng@outlook.com>
2023-08-15 21:44:02 +0800

    modify csv2db.py

--- Python/Script/Others/csv2db.py
+++ Python/Script/Others/csv2db.py
@@ -2,20 +2,24 @@ import csv
 import sqlite3
 
 
+# 数据库的路径
+database_path = "C:/Users/hrzhe/Documents/课程/大一下/可视化/" \
+                "raw_data.db"
 # 存放数据集的文件夹
-dataset_directory = "C:/Users/hrzhe/Documents/课程/大一下/可视化/Datasets/"
-# 数据库的名称
-database_name = "database.db"
-
+dataset_dir = "C:/Users/hrzhe/Documents/课程/大一下/可视化/Datasets/"
 # 数据集的名称
-dataset_name = "Environment_LandCover_E_All_Data.csv"
-# 存放表名，如 food_prices，不可包含空格
-tabel_name = "population"
-# 依次存放列名，不可有空格
-columns = ["Area_Code","Area_Code_M49","Area","Item_Code","Item_Code_CPC","Item","Element_Code","Element","Unit","Y2010","Y2010F","Y2011","Y2011F","Y2012","Y2012F","Y2013","Y2013F","Y2014","Y2014F","Y2015","Y2015F","Y2016","Y2016F","Y2017","Y2017F","Y2018","Y2018F","Y2019","Y2019F","Y2020","Y2020F"]
-# 依次存放列的数据类型，0代表text，1代表integer，2代表real
-t = [1,0,0,1,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
+dataset_name = "ConsumerPriceIndices_E_All_Data.csv"
+# 存放表名，不可包含空格
+tabel_name = "consumer_price_index"
 
+# 依次存放字段名称，不可包含空格
+fields = ["Area_Code","Area_Code_M49","Area","Item_Code","Item","Element_Code","Element","Months_Code","Months","Unit"]
+for i in range(2000, 2023):
+    fields.append("Y" + str(i))
+    fields.append("Y" + str(i) + "F")
+
+# 依次存放字段数据类型
+t = []
 
 types = []
 for i in t:
@@ -26,24 +30,24 @@ for i in t:
     elif i == 2:
         types.append("real")
 
-conn = sqlite3.connect(dataset_directory + database_name)
+conn = sqlite3.connect(database_path)
 c = conn.cursor()
 
 sql_create = "CREATE TABLE IF NOT EXISTS " + tabel_name + " ("
-for i in range(len(columns)):
-    sql_create += columns[i] + " " + types[i] + ","
+for i in range(len(fields)):
+    sql_create += fields[i] + " " + types[i] + ","
 sql_create = sql_create[:-1] + ")"
 c.execute(sql_create)
 
 sql_insert = "INSERT INTO " + tabel_name + " VALUES ("
-for i in range(len(columns)):
+for i in range(len(fields)):
     sql_insert += "?,"
 sql_insert = sql_insert[:-1] + ")"
 
-with open(dataset_directory + dataset_name) as fid:
+with open(dataset_dir + dataset_name) as fid:
     cr = csv.DictReader(fid)
     for row in cr:
-        values = (lambda *args: args)(*row.values())[:len(columns)] # type: ignore
+        values = (lambda *args: args)(*row.values())[:len(fields)] # type: ignore
         c.execute(sql_insert, values)
 
 conn.commit()
